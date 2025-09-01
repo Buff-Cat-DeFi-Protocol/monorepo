de4840ee283674a230013d7bf33d0a7f4ed20985
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-01 15:07:26 +0530

    feat(contract: eth): added necessary imports and inheritance

--- contracts/ethereum/src/Buffcat.sol
+++ contracts/ethereum/src/Buffcat.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Buffcat {
+import "@openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/utils/PausableUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract Buffcat is
+    Initializable,
+    OwnableUpgradeable,
+    UUPSUpgradeable,
+    PausableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    using SafeERC20 for IERC20;
+
     // Variables :-
     
     // Events :-
