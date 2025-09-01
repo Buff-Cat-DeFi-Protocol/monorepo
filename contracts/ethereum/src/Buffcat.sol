7bc398daebfa5c877ae3b5a4814c9d5007ecc8d1
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-01 14:44:59 +0530

    deleted boilerplate contract and made buffcat.sol

---
+++ contracts/ethereum/src/Buffcat.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Buffcat {
+    // Variables :-
+    
+    // Events :-
+
+    // Errors :-
+
+    // Functions :-
+    // 1. Inherited Functions :-
+    // 2. Public Functions :-
+    // 3. Private Functions :-
+    // 4. Admin Functions :-
+}
