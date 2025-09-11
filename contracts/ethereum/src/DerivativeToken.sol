2201c3d88aa76308ab69e3a748e51f6faf907be2
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-11 22:41:47 +0530

    feat(contract: eth): made implementation contract for derivatives

---
+++ contracts/ethereum/src/DerivativeToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20Upgradeable} from "@openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20BurnableUpgradeable} from "@openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract MyToken is 
+    Initializable, 
+    ERC20Upgradeable, 
+    ERC20BurnableUpgradeable, 
+    OwnableUpgradeable
+{
+    uint8 private customDecimals;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, string calldata name, string calldata symbol, uint8 decimals) public initializer {
+        __ERC20_init(name, symbol);
+        __ERC20Burnable_init();
+        __Ownable_init(initialOwner);
+        customDecimals = decimals;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return customDecimals;
+    }
+}
