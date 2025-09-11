a3db2646f77f4abea8acad16b747fa23f9f2b44b
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-11 22:42:30 +0530

    feat(contract: eth): written necessary variables, mappings & constants

--- contracts/ethereum/src/Buffcat.sol
+++ contracts/ethereum/src/Buffcat.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin-contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin-contracts-upgradeable/utils/PausableUpgradeable.sol";
-import "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {OwnableUpgradeable} from "@openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin-contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin-contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {Initializable} from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {IERC20} from "@openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {Clones} from "@openzeppelin-contracts/proxy/Clones.sol";
 
 contract Buffcat is
     Initializable,
@@ -18,7 +19,22 @@ contract Buffcat is
 {
     using SafeERC20 for IERC20;
 
+    // Constants :-
+    uint256 public MIN_LOCK_VALUE;
+
     // Variables :-
+    address public derivativeImplementation;
+
+    address public developerWallet;
+    address public founderWallet;
+    mapping(address => bool) public authorizedUpdaters;
+
+    uint256 public feePercentage;
+    FeeDistribution public feeSplit;
+
+    mapping(address => bool) public whitelistedTokens;
+    mapping(address => address) tokenDerivatives;
+
     
     // Events :-
 
