8140bd01b89d34f0e5f2d06ad959bd16da0be1f0
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-10-21 14:39:10 +0530

    feat(web): made header component

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -5,32 +5,75 @@ import ThemedButton from "@/components/themed/button";
 import { ChevronRight } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import Image from 'next/image';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { blockchains } from "@/constants/blockchains";
+import { selectedBlockchainAtom } from "@/store/global";
+import { useSetAtom } from "jotai";
 
 export const Header: React.FC = () => {
     const pathname = usePathname();
     const router = useRouter();
+    const setBlockchainAtom = useSetAtom(selectedBlockchainAtom);
 
     return (
         <>
-            <div className="h-22 w-full border-[1px] border-gray-800 px-12 flex items-center justify-between">
+            <div className="h-22 w-full border-[1px] border-gray-800 px-12 
+            flex items-center justify-between bg-custom-root-bg">
                 <div className="flex items-center">
-                    <Image src="/twoside-bold.png" height={56} width={56} />
+                    <Image src="/twoside-bold.png" alt="Twoside Logo" height={56} width={56} />
                     <Link className="no-underline ms-2" href={"/"}>
                         <span className={typography.h1}>TWOSIDE</span>
                     </Link>
                 </div>
-                <ThemedButton
-                    style="primary"
-                    variant="outline"
-                    size="lg"
-                    className="w-50 h-10 rounded-full flex items-center"
-                    onClick={() => {
-                        pathname == "/" ? router.push('/dashboard') : router.push('/')
-                    }}
-                >
-                    <span className={typography.h4}>{pathname == "/" ? "Launch dApp" : "Go Home"}</span>
-                    <ChevronRight size />
-                </ThemedButton>
+                <div className="flex items-center gap-6">
+                    <Select>
+                    <SelectTrigger 
+                    className="w-[180px] cursor-pointer shadown-none
+                    border border-custom-primary-color rounded-2xl"
+                    >
+                        <SelectValue placeholder="Select a blockchain" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Blockchains</SelectLabel>
+                            {blockchains.map(b => {
+                                return (
+                                    <>
+                                    <SelectItem
+                                    onClick={() => {
+                                        setBlockchainAtom(b)
+                                    }}
+                                    className="cursor-pointer"
+                                    value={b.id}>
+                                        {b.name}
+                                    </SelectItem>
+                                    </>
+                                );
+                            })}
+                        </SelectGroup>
+                    </SelectContent>
+                    </Select>
+                    <ThemedButton
+                        style="primary"
+                        variant="outline"
+                        size="lg"
+                        className="h-10 px-6 rounded-full flex items-center gap-2"
+                        onClick={() => {
+                            pathname == "/" ? router.push('/dashboard') : router.push('/')
+                        }}
+                    >
+                        <span className={typography.h4}>{pathname == "/" ? "Launch dApp" : "Go Home"}</span>
+                        <ChevronRight />
+                    </ThemedButton>
+                </div>
             </div>
         </>
     );
